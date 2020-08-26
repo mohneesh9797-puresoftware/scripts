7867e2b29ad10508d55009ba95a3db0438aee198
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-08-26 14:04:26 +0530

    Update install-common-toolchain.sh

--- ci/install-common-toolchain.sh
+++ ci/install-common-toolchain.sh
@@ -52,9 +52,8 @@ fi
         if [ `uname -m` = 'aarch64' ]; then
    		sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libffi-dev liblzma-dev git
 		git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-		echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-		echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-		echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+		export PYENV_ROOT="$HOME/.pyenv"
+		export PATH="$PYENV_ROOT/bin:$PATH"
 		pyenv --version
 	fi	
 	pyenv versions
