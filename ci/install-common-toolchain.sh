5e48e046b18651ccfd150324031ab77713494923
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-08-26 13:43:25 +0530

    Update install-common-toolchain.sh

--- ci/install-common-toolchain.sh
+++ ci/install-common-toolchain.sh
@@ -49,7 +49,16 @@ fi
 
     # On Linux Travis, System Python is 2.7.X, use `pyenv` to pick up Python 3.6.7
     if [ "${OS}" = "linux" ]; then
-        pyenv versions
+        if [ `uname -m` = 'aarch64' ]; then
+   		sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libffi-dev liblzma-dev git
+		git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+		echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+		echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+		echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+		exec $Shell
+		pyenv --version
+	fi	
+	pyenv versions
         pyenv global 3.6.7
     fi    
 
