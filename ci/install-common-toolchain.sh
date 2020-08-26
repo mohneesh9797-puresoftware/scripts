20ec7229d1ca8a051436923d35e088f18585f19c
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-08-26 14:02:06 +0530

    Update install-common-toolchain.sh

--- ci/install-common-toolchain.sh
+++ ci/install-common-toolchain.sh
@@ -55,7 +55,6 @@ fi
 		echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 		echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 		echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-		exec $Shell
 		pyenv --version
 	fi	
 	pyenv versions
